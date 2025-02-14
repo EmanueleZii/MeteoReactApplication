72af33d1a97543f2bc9886f26961175790def833
unknown <zignale92@gmail.com>
2025-02-14 11:47:37 +0100

    classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement(){
+    this.setState((curState) =>{
+      return {count: curState.count - 1}
+    });
+  }
+
+  handleIncrement(){
+    this.setState((curState) =>{
+      return {count: curState.count + 1}
+    });
+  }
+
+ render () {
+
+  const date = new Date("June 21 2027");
+
+  date.setDate(date.getDate() + this.state.count);
+
+  return <div>
+    <button onClick={this.handleDecrement}>-</button>
+    <span>{date.toDateString()}</span>
+    <button onClick={this.handleIncrement}>+</button>
+  </div>
+ }
 }
 
-export default App;
+export default Counter;
