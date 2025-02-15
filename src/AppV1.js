7ded4926e5cd009d1c90e5688f831c2f2b500f92
unknown <zignale92@gmail.com>
2025-02-15 16:30:40 +0100

    finish

--- src/AppV1.js
+++ src/AppV1.js
@@ -36,7 +36,7 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: 'lisbon', 
+    location: '', 
     isLoading: false, 
     displayLocation: '', 
     weather: {}
@@ -47,6 +47,10 @@ class App extends React.Component {
   }
 
   fetchWeather = async () => {
+    
+    if (this.state.location.length < 2 ) 
+      return this.setState({weather: {}});
+
     try {
       this.setState({isLoading: true });
       // 1) Getting location (geocoding)
@@ -70,7 +74,7 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({weather: weatherData.daily});
     } catch (err) {
-      console.err(err);
+      console.error(err);
     }finally{
       this.setState({isLoading: false});
     }
@@ -78,12 +82,23 @@ class App extends React.Component {
 
   setLocation = (e) => this.setState({location: e.target.value})
 
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem('location') || '' });
+  } 
+
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem('location', this.state.location);
+    }
+  }
+
   render()
   {
     return <div className='app'>
       <h1>Classy Weather</h1>
       <Input location={this.state.location} onChangeLocation={this.setLocation} />
-      <button onClick={this.fetchWeather}>Get weather</button>
     
       {this.state.isLoading && <p className='loader'>Loading...</p>}
     
@@ -112,6 +127,11 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  
+  componentWillUnmount(){ 
+    console.log('Weather component unmounted');
+  }
+
   render() {
     const {
       temperature_2m_max: max,
